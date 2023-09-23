a50b344e641e5bd6db5829fcc4dd90a329e6ace9
Tsotne Tsirekidze <tsirekidze022@gmail.com>
2023-09-23 19:42:28 +0400

    banner is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header/Header";
+import Main from "@/components/Main/Main";
 
 export default function Home() {
   return (
     <div style={{ width: "100%" }}>
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ components/Main/Main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Main.scss";
+
+const Main = () => {
+  return (
+    <main className="main">
+      <div className="main-content">
+        <div className="banner">
+          <h1 className="banner-title">გაყიდე და იყიდე დომენი მარტივად</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
